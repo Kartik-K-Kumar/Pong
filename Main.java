7d197808e43a90aa12b1e5b47bacabfdcd04bd8a
Kartik-K-Kumar <kartikkamalkumar5@gmail.com>
2025-03-11 07:34:35 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,13 +12,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public void start(Stage stage) {
-        stage.setTitle("Agar.io");
+        stage.setTitle("Pong");
 
         Group root = new Group();
         Player1 player1 = new Player1(100.0, 35.0, 900, 550);
+        Player2 player2 = new Player2(100.0, 35.0, 100, 550);
 
         Rectangle playerRectangle = player1.createRectangle();
         root.getChildren().add(playerRectangle);
+        Rectangle player2Rectangle = player2.createRectangle();
+        root.getChildren().add(player2Rectangle);
 
         Scene scene = new Scene(root, 1000, 700);
 
@@ -27,17 +30,22 @@ public class Main extends Application {
             public void handle(KeyEvent keyEvent) {
                 switch (keyEvent.getCode()) {
                     case UP:
-                        player1.setY(player1.getY() - 10); 
+                        player1.setY(player1.getY() - 10);
                         break;
                     case DOWN:
-                        player1.setY(player1.getY() + 10); 
+                        player1.setY(player1.getY() + 10);
+                        break;
+                    case W:
+                        player2.setY(player2.getY() - 10);
+                        break;
+                    case S:
+                        player2.setY(player2.getY() + 10);
                         break;
                     default:
                         break;
                 }
-
-                
                 playerRectangle.setY(player1.getY());
+                player2Rectangle.setY(player2.getY());
             }
         });
 
